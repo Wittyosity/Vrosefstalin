b2c9cfaa910495e293f68d12d2b8d9dd9a7d32e2
Lloydprogramming <65378999+Lloydprogramming@users.noreply.github.com>
2020-05-28 02:15:51 +0000

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -44,6 +44,7 @@ int main(){
 	string name;
 	int budget=0;
 	int id;
+	int layerCount = 0;
 	int itemID=1;
 	item* holder;
 	
@@ -59,79 +60,143 @@ int main(){
 	cin >> budget;
 	shopper = new person(name, budget);
 
-	cout << "Here are the stalls we have at the market today: " <<endl;
-	cout << "ID:\t" << "Name:" << endl;
-	cout << one->getID() << "\t" << one->getName() << endl;
-	//one->printList();
-	cout << two->getID() << "\t" << two->getName() << endl;
-	//two->printList();
-	cout << three->getID() << "\t" << three->getName() << endl;
-	//three->printList();
+	//Purchase loop needs to start
 
+	cout << endl;
 
-		cout << "Please enter the ID of the stall you wish to visit, or enter 0 if you are finished shopping. " << endl;
+while(budget>=0){
+	if(layerCount==0) {
+		cout << "Here are the stalls we have at the market today: " <<endl;
+		cout << "ID:\t" << "Name:" << endl;
+		cout << one->getID() << "\t" << one->getName() << endl;
+		//one->printList();
+		cout << two->getID() << "\t" << two->getName() << endl;
+		//two->printList();
+		cout << three->getID() << "\t" << three->getName() << endl;
+		//three->printList();
+
+		cout << "Please enter the ID of the stall you wish to visit," << endl;
+		cout << "or enter 0 if you are finished shopping." << endl;
 		// cout << "Enter 1 for the " << one->getName() << " stall, 2 for the " << two->getName() 
 		// << " stall, or 3 for the "<< three->getName() <<"."<< endl;
+		
 		cin >> id;
-		switch (id) {
-			case 1:
-			//display stall 1
-			cout <<"Welcome to the " << one->getName() << " stall, today we have:" <<endl;
-			one->printList();
-			break;
-			case 2:
-			//display stall 2, enable purchasing from stall
-			cout <<"Welcome to the " << two->getName() << " stall, today we have:" <<endl;
-			two->printList();
-			break;
-			case 3:
-			//display stall 3
-			cout <<"Welcome to the " << three->getName() << " stall, today we have:" <<endl;
-			three->printList();
-			break;
+
+		if(id==0){
+			cout << endl;
+			cout << "You have finished shopping." << endl;
+			// Receipt system output.
+			return 0;
+			}
+
+		layerCount++;
+		cout << endl;
+
 		}
-		
-
-		switch(id) {
-			//ensure that the function repeats while the person wishes to remain at the same stall
+	else if(layerCount==1){
 			
+			switch (id) {
+
+				case 0:
+					layerCount = layerCount -1;
+				break;
+
 				case 1:
-				//purchase from stall 1
-				cout <<"Please enter the ID of the item you wish to purchase. Or enter 0 if you wish to move to another stall." << endl;
-				cin >> itemID;
-				if(isdigit(itemID)){
-			
-				switch (itemID) {
-					case 0:
-						break;
-						break;
-						break;
-					case 1:
-					//move the item from the stall to the person's 'cart'
-						holder = one->getItem(itemID);
-						shopper->addItem(holder);
-						cout << "You have purchased a " << endl;
+				//display stall 1
+					cout <<"Welcome to the " << one->getName() << " stall, today we have:" <<endl;
+					one->printList();
+					layerCount++;
+					cout << endl;
+				break;
 
-						shopper->printCart();
-						break;
-					case 2:
-						holder = one->getItem(itemID);
-						shopper->addItem(holder);
-						cout << "you have purchased a " << endl;
-						shopper->printCart();
-						break;
-					case 3:
-						holder = one->getItem(itemID);
-						shopper->addItem(holder);
-						cout << "you have purchased a " << endl;
-						shopper->printCart();
-						break;
-					}
-				}else{
-					cout << "invalid input, please input a valid ID" << endl;
-					
+				case 2:
+				//display stall 2, enable purchasing from stall
+					cout <<"Welcome to the " << two->getName() << " stall, today we have:" <<endl;
+					two->printList();					
+					layerCount++;
+					cout << endl;
+				break;
+
+				case 3:
+				//display stall 3
+					cout <<"Welcome to the " << three->getName() << " stall, today we have:" <<endl;
+					three->printList();				
+					layerCount++;
+					cout << endl;
+				break;
+
+				default:
+					cout << "Invalid input" << endl;
+					layerCount = layerCount -1;
+					return 0;
+				break;
 				}
-		
+			}
+
+	else if(layerCount==2){
+
+				//ensure that the function repeats while the person wishes to remain at the same stall
+					switch(id) {
+
+						case 0:
+							layerCount = layerCount -2;
+						break;
+									
+						case 1:
+						//purchase from stall 1
+						cout <<"Please enter the ID of the item you wish to purchase," << endl;
+						cout << "or enter 0 if you wish to move to another stall." << endl;
+
+						cin >> itemID;
+
+							switch (itemID) {
+
+							case 0:
+								layerCount = layerCount -2;
+							break;
+
+							case 1:
+							//move the item from the stall to the person's 'cart'
+								holder = one->getItem(itemID);
+								shopper->addItem(holder);
+								cout << endl;
+								cout << "Your cart currently contains... " << endl;
+
+								shopper->printCart();
+								cout << endl;
+							break;
+
+							case 2:
+								holder = one->getItem(itemID);
+								shopper->addItem(holder);
+								cout << endl;
+								cout << "you have purchased a " << endl;
+								shopper->printCart();
+								cout << endl;
+							break;
+
+							case 3:
+								holder = one->getItem(itemID);
+								shopper->addItem(holder);
+								cout << endl;
+								cout << "you have purchased a " << endl;
+								shopper->printCart();
+								cout << endl;
+							break;
+
+							default:
+								cout << "invalid input, please input a valid ID" << endl;
+								layerCount = layerCount -1;
+								cout << endl;
+							break;
+
+							}
+						}
+					}
+		if(budget<=0){
+		cout << "You are out of money." << endl;
+		//print reciept
+		}
 	}
 
 
